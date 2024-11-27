2b7f3d307c234e30fa7b6d90f1c584ee6258280a
Can <bayramcan1515@gmail.com>
2024-11-27 18:22:03 +0300

    ı developed the guess number game,guess the number between 1-40

---
+++ moduleguessnumber.py
@@ -0,0 +1,30 @@
+import random
+import time
+
+print("WELCOME THE GUESS NUMBER GAME!\nguess the number between 1-40\n")
+
+myrandomnumber=random.randint(1,40)
+rightofprediciton=5
+print("your right of prediction:",rightofprediciton)
+
+while True:
+    prediciton=int(input("prediction:"))
+    if(prediciton<myrandomnumber):
+        print("informations are controlling...")
+        time.sleep(2)
+        print("please,say number is more than yours")
+        rightofprediciton-=1
+    elif(prediciton>myrandomnumber):
+        print("informations are controlling...")
+        time.sleep(2)
+        print("please,say number is less than yours")
+        rightofprediciton -= 1
+    else:
+        print("informations are controlling...")
+        time.sleep(2)
+        print("Congratulations!\nnumber:",myrandomnumber)
+        break
+    if(rightofprediciton==0):
+        print("You are out of predictions.")
+        print("number:",myrandomnumber)
+        break
